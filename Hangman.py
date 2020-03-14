1fc36aa842b5407b8eb3a5c9c6060852fbefa6c7
Alejandro Penaloza <alejandro.penaloza.20@gmail.com>
2020-03-14 17:07:43 -0400

    Hangman original file upload

---
+++ Hangman.py
@@ -0,0 +1,6 @@
+#THIS IS HANGMAN
+print('"Hangman"\nA game where you will try to guess which the hidden word is!')
+print('\n\n')
+word = input('Input the word to guess:\n')
+print(word)
+number_of_letters = len(word)
